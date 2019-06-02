1c9eab6319ee93c243f8ee940ff4fa84dd308dfe
sarL3y <matt.tapper@gmail.com>
2019-06-02 16:58:22 -0700

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './App';
+import Card from './Card';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
