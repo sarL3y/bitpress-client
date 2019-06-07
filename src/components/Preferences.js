b28fb085985f086135900632c913e0c307d3277a
sarL3y <matt.tapper@gmail.com>
2019-06-07 00:49:48 -0700

    Add Profile // Update styling

---
+++ src/components/Preferences.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Topics from './Topics';
+
+export default function Preferences() {
+    return (
+        <div className="container-topics">
+            <h4>Topics</h4>
+            <Topics />
+        </div>
+    );
+};
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar';
 
+import Preferences from './Preferences';
+
+import './Profile.scss';
+
 const Profile = props => {
     return (
         <main>
             <Navbar {...props} />
-            <div>
+            <div className="container-profile">
                 <h2>BitPress Profile</h2>
                 <p>Lorem Ipsum</p>
+                <h3>Preferences</h3>
+                <Preferences />
             </div>
         </main>
     );
