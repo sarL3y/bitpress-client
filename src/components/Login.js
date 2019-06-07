2743d01378b2f8a4853f8514b065afcec03168f4
sarL3y <matt.tapper@gmail.com>
2019-06-07 00:13:38 -0700

    Login working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from '../firebase';
+
 import Navbar from './Navbar';
 
 const Login = props => {
+
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    async function login() {
+        try {
+            await firebase.login(email, password);
+            props.history.replace("/dashboard");
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+    
     return (
         <main>
             <Navbar {...props} />
-            <div>
+            <div className="container-login">
                 <h2>BitPress Login</h2>
+                <form onSubmit={e => e.preventDefault() && false}>
+                    <div className="form-input-email">
+                        <input 
+                            placeholder="Email"
+                            type="email"
+                            value={email}
+                            aria-label="uemail"
+                            onChange={e => setEmail(e.target.value)}
+                            />
+                    </div>
+                    <div className="form-input-password">
+                        <input 
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            aria-label="password"
+                            onChange={e => setPassword(e.target.value)}
+                            />
+                    </div>
+
+                    <button type="submit" className="button" onClick={login}>
+                    Login
+                    </button>
+                </form>
                 <p>Lorem Ipsum</p>
             </div>
         </main>
