d15cd48844c243e1bcfcf784a910c229cb92788f
sarL3y <matt.tapper@gmail.com>
2019-07-07 22:12:02 -0700

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/Card';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
