00c3d58aa91e729d2141f29e0187ddf49df1004c
sarL3y <matt.tapper@gmail.com>
2019-08-15 10:45:14 -0700

    Add additional tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
-import Profile from './components/Profile';
 import App from './App';
+import Profile from './components/Profile';
+import Register from './components/Register';
+import Login from './components/Login';
+import HomePage from './components/HomePage';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
 
 window.alert = jest.fn();
 
@@ -34,3 +39,48 @@ describe("<Profile /> tests", () => {
 		expect(container.exists()).toBe(true);
 	})
 });
+
+describe("<Register /> tests", () => {
+	const container = shallow(<Register history="/login" />);
+
+	it("should render Register", () => {
+		
+		expect(container.exists()).toBe(true);
+	})
+});
+
+describe("<Login /> tests", () => {
+	const container = shallow(<Login history="/login" />);
+
+	it("should render Login", () => {
+		
+		expect(container.exists()).toBe(true);
+	})
+});
+
+describe("<HomePage /> tests", () => {
+	const container = shallow(<HomePage history="/login" />);
+
+	it("should render HomePage", () => {
+		
+		expect(container.exists()).toBe(true);
+	})
+});
+
+describe("<Dashboard /> tests", () => {
+	const container = shallow(<Dashboard history="/login" />);
+
+	it("should render Dashboard", () => {
+		
+		expect(container.exists()).toBe(true);
+	})
+});
+
+describe("<Navbar /> tests", () => {
+	const container = shallow(<Navbar />);
+
+	it("should render Navbar", () => {
+
+		expect(container.exists()).toBe(true);
+	})
+})
